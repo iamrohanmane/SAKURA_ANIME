207ec9902a5b664b7ef23b72fc50dde6098df76b
iamrohanmane <ronymane9841@gmail.com>
2024-03-12 10:52:15 +0530

    bug fixex

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,7 +53,7 @@ const userSchema = new Schema(
 );
 
 userSchema.pre("save", async function (next) {
-  if (this.isModified("Password")) return next();
+  if(this.isModified("password")) return next();
 
   this.password = await bcrypt.hash(this.password, 100);
   next();
@@ -68,7 +68,7 @@ userSchema.methods.generateAccessToken = function () {
     {
       _id: this._id,
       email: this.email,
-      username: this.username,
+      userName: this.userName,
       fullName: this.fullName,
     },
     process.env.ACCESS_TOKEN_SECRET,
